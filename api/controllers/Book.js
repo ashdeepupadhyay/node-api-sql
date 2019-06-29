8c4f9061498b6f9f4c3665b4f112f4f4c1cc6368
ashdeep <ashdeepupadhyay@gmail.com>
2019-06-30 01:22:07 +0530

    post request working to updates book

--- api/controllers/Book.js
+++ api/controllers/Book.js
@@ -1,5 +1,9 @@
 const Pool = require('pg').Pool
 const dotenv = require('dotenv');
+
+
+// parse application/json
+
 dotenv.config();
 
 const pool = new Pool({
@@ -28,7 +32,6 @@ exports.getBookList = (request, response,next) => {
         text: "select book_id,book_name,book_author,rent_date,no_of_release_days,init_days_price,fixed_days_price,category_name as category from public.book b inner join public.category cat on cat.category_id=b.category_id where client_id=$1",
         values: [req.params.clientID]
     }
-      //const id =req.params.clientID;
       pool.query(query, (error, results) => {
         if (error) {
           throw errors
@@ -36,3 +39,37 @@ exports.getBookList = (request, response,next) => {
         res.status(200).json(results.rows)
       })
   }
+
+exports.updateBookReturn = (req,res,next)=>{
+    const query={
+        text : "update public.book set rent_date=null,client_id=null where book_id=$1",
+        values:[req.body.bookid]
+    }
+    pool.query(query,(error,results)=>{
+        if(error){
+            throw errors
+        }
+        res.status(200).json(results.rows)
+    })
+}
+
+exports.UPDATE_REGISTRATION_DATE_QUERY = (req,res,next)=>{
+    var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+var yyyy = today.getFullYear();
+today=yyyy+'-'+mm+'-'+dd;
+console.log(today);
+console.log(req.body.clientid);
+
+    const query={
+        text:"update public.book set rent_date=$1,client_id=$2 where book_id=$3",
+        values:[today,req.body.clientid,req.body.bookid]
+    }
+    pool.query(query,(error,results)=>{
+        if(error){
+            throw error
+        }
+        res.status(200).json(results.rows);
+    })
+}
--- api/routes/Book.js
+++ api/routes/Book.js
@@ -5,5 +5,6 @@ const bookController = require('../controllers/Book');
 
 router.get('/notRented',bookController.getBookList);
 router.get('/:clientID',bookController.getBooksForClient);
-
+router.post('/',bookController.updateBookReturn);
+router.post('/selectBook',bookController.UPDATE_REGISTRATION_DATE_QUERY)
 module.exports = router;
